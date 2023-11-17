7ab0395a572a487f4ca97ce67c6b815b6639bdbb
Build Mount <47172782+Build-Mount@users.noreply.github.com>
2023-11-17 21:17:33 +0900

    Add files via upload

---
+++ addEventListener.js
@@ -0,0 +1,5 @@
+document.getElementById("btn").addEventListener("click", btnClick);
+
+function btnClick() {
+    document.getElementById("output").textContent = document.getElementById("value").value;
+};
